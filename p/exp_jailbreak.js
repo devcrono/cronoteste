d1ee0b2fde355f2f53cb78f9bda8556d0d8995af
devcrono <69425591+devcrono@users.noreply.github.com>
2020-09-08 14:48:16 -0400

    600

--- p/exp_jailbreak.js
+++ p/exp_jailbreak.js
@@ -56,7 +56,7 @@ var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
 if(main_ret == 179 || main_ret == 0){
 	setCookie("JB","DONE",1);
-	setTimeout(function(){document.getElementById("msgs").innerHTML="AUTO JAILBREAK REALIZADO COM SUCESSO!!, AGORA ESCOLHA HEN OU MIRA!"; }, 400);
+	setTimeout(function(){document.getElementById("msgs").innerHTML="AUTO JAILBREAK REALIZADO COM SUCESSO!!, AGORA ESCOLHA HEN OU MIRA!"; }, 600);
 } else{
 	prompt("Falha ao Realizar o Auto Jailbreak! Favor Reiniciar o PS4 e Tentar Novamente!!");
 }}else{alert("Execução Incompleta, Feche e Abra o Navegador Novamente!!");}
