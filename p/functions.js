fb36d0d2b72a52b6db24b64e333bbeea62d72e0e
devcrono <69425591+devcrono@users.noreply.github.com>
2020-09-09 10:06:45 -0400

    1500/600

--- p/functions.js
+++ p/functions.js
@@ -1,4 +1,5 @@
 
+
 function load_JB()
 {	var jbDone=getCookie("JB");
 	var spoofed=navigator.userAgent.indexOf("6.72")>=0 ? false : true;
@@ -12,7 +13,7 @@ function load_JB()
 
 function exploit(){
 	document.getElementById("msgs").innerHTML="EXECUTANDO AUTO JAILBREAK AGUARDE!!";
-	setTimeout(function(){jb();}, 1500);
+	setTimeout(function(){jb();}, 600);
 }
 
 
